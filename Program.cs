d9e95230cb6805f05adb2ecd43d774f9e5460bcd
riya more <you@example.com>
2023-02-19 15:45:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿//namespace HelloWorld
+//{
+//class program
+//{
+
+
+//public static void Main(string[] args)
+//{
+//int num = 7;
+
+//double num1 = 10.9;
+//bool val = false;
+//char singleCharacter = 'r';
+//string name = "BridgeLabz";
+//Console.WriteLine("Hii,Hello how are you?");
+//Console.WriteLine("This is an Integer value = " + num);
+//Console.WriteLine("This is an double value = " + num1);
+//Console.WriteLine("This is an boolean value = " + val);
+//Console.WriteLine("This is an Charcter = " + singleCharacter);
+//Console.WriteLine("This is an string value = " + name);
+
+//string nameofperson = Console.ReadLine();
+
+//Console.WriteLine("This is the " + nameofperson);
+//Console.WriteLine("The name is {0} {1} {2} {3}" ,nameofperson,num,num1,val);
+
+/*Console.WriteLine("Print the username");
+string username = Console.ReadLine();
+
+int val = username.Length;
+
+Console.WriteLine("The number of characters is = " + val);
+
+if(username.Length>3  && username.Length<5)
+{
+    Console.WriteLine("Hello " + username + " , How are you? ");
+    Console.WriteLine("Hello {0},How are you?", username);
+
+}
+else if(username.Length<5)
+{
+    Console.WriteLine("The name is not having more than 5 characters");
+}
+else
+{
+    Console.WriteLine("This name is having min 3 characters");
+}
+
+Console.WriteLine("Print the username");
+string username = Console.ReadLine();
+
+int val = username.Length;
+
+Console.WriteLine("The number of characters is = " + val);
+
+if(username.Length>3)
+{
+    Console.WriteLine("Hello " + username + " , How are you? ");
+    Console.WriteLine("Hello {0},How are you?", username);
+
+}
+else
+{
+    Console.WriteLine("The name is not having max 3 characters");
+}*/
+
+/* Console.WriteLine("Enter the first number: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+ Console.WriteLine("Enter the second number:");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+
+ int num3 = num1 + num2;
+ Console.WriteLine("The addition of two numbers = " + num3);*/
+
+int year;
+Console.Write("Enter an year :  ");
+year = Convert.ToInt32(Console.ReadLine());
+
+if ((year % 400) == 0)
+    Console.WriteLine("{0} is a leap year.", year);
+else if ((year % 100) == 0)
+    Console.WriteLine("{0} is not a leap year.", year);
+else if ((year % 4) == 0)
+    Console.WriteLine("{0} is a leap year.", year);
+else
+    Console.WriteLine("{0} is not a leap year.", year);
+
+
+
+
+
+
+
+/*}
+}   
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ username.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Flipcoin
+    {
+        public static void CalculatePercentage()
+        {
+            Console.WriteLine("pplease enter the value for number of flips");
+            int head = 0;
+            int tail = 0;
+            int headcount = 0;
+            int tailcount = 0;
+
+            double headprcentage;
+            double tailprcentage;
+
+            int flipcoin=Convert.ToInt32(Console.ReadLine());
+
+            if(flipcoin>0)
+            {
+                for(int i=0;i<flipcoin; i++)
+                {
+                    Random random=new Random();
+                    int number = random.Next(2);
+
+                    Console.WriteLine("The Random number generated is " + number);
+
+                    if (number ==0)
+                    {
+                        tail++;
+
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+                }
+                Console.WriteLine("The tail count is " + tailcount);
+                Console.WriteLine("The head count is " + headcount);
+
+                headpercentage = headcount * 100 / flipcoin;
+                TailPercentage = tailcount * 100 / flipcoin;
+
+                Console.WriteLine("HeadPercentage : " + headpercentage + "%");
+                Console.WriteLine("Tail percentage : " + TailPercentage + "%");
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid number");
+            }
+
+                
+        
+        }
+        
+
+    }
+}
